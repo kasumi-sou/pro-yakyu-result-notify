d6364fd37e414d07138560584dfdbe4641b2e8cf
kasumi-sou <110779036+kasumi-sou@users.noreply.github.com>
2025-05-12 20:36:23 +0900

    chore: add config files

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+// .eslint.config.mjs
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+    },
+  }
+);
